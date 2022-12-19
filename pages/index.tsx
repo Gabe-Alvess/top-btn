a9f581649dbc8a52891b1a0a3e1a308a5398389f
Biel-Alves <gabriel_vieira2018@hotmail.com>
2022-12-19 10:36:51 +0100

    closed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
-import { AboutPage, ContactPage, HomePage, ProjectsPage, Header, TopButton } from "../components";
+import {
+  AboutPage,
+  ContactPage,
+  HomePage,
+  ProjectsPage,
+  Header,
+  TopButton,
+} from "../components";
 import type { NextPage } from "next";
 import { useRef } from "react";
 import Head from "next/head";
+
+// StackOverflow: https://stackoverflow.com/questions/70808951/how-to-get-window-scrolltop-scrolly-or-any-other-distance-value-when-using-css
+// YouTube: https://www.youtube.com/watch?v=Xz2Z8xKH-R0&t=811s
 
 const Home: NextPage = () => {
   const scrollRef: any = useRef();
